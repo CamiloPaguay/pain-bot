cfd437c43f1191274844d01683709252106b4115
CamiloPaguay <164214564+CamiloPaguay@users.noreply.github.com>
2026-09-03 14:49:37 -0500

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -1,24 +1,7 @@
-let handler = async (m, { conn, args, participants, isAdmin, isBotAdmin, isOwner, isPrems, usedPrefix, command }) => {
-  try {
-    return conn.sendMessage(m.chat, {
-      text: '👋 Hola Mundo',
-      contextInfo: {
-        ...rcanal.contextInfo
-      }
-    }, { quoted: m })
-  } catch (e) {
-    console.error('Error en holamundo:', e)
-    return conn.sendMessage(m.chat, {
-      text: '[❌] Ocurrió un error al enviar el mensaje.',
-      contextInfo: {
-        ...rcanal.contextInfo
-      }
-    }, { quoted: m })
-  }
+let handler = async (m, { conn }) => {
+  await conn.sendMessage(m.chat, { text: '👋 Hola Mundo' }, { quoted: m })
 }
 
-handler.help = ['#holamundo']
-handler.tags = ['fun']
 handler.command = ['menu', 'menus']
 
 export default handler
